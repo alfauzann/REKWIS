8c0dbcd4e6e410d7164f6266e3d35d43c1f0d541
Nayla putri asyifa <naylaputriasyifa@Naylas-MacBook-Air.local>
2022-06-22 00:43:53 +0700

    coba

---
+++ lib/widgets/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  //Primary color
+  static var primaryColor = const Color.fromARGB(122, 1, 1, 2);
+  static var blackColor = const Color.fromARGB(255, 0, 0, 0);
+}
